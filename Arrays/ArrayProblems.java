b4a5bdbf1deb3a53f0c7a062fdf1fa6cdc6b685f
shivaragula <148362743+shivaragula@users.noreply.github.com>
2025-07-03 21:32:34 +0530

    Day 1: Added ArrayProblems.java

---
+++ Arrays/ArrayProblems.java
@@ -0,0 +1,8 @@
+public class ArrayProblems {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30};
+        for (int num : arr) {
+            System.out.println(num);
+        }
+    }
+}
