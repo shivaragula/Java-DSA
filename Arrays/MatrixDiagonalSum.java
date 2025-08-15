27cf454b762f3bf8e7e91a1f58d925da059d27fa
shivaragula <148362743+shivaragula@users.noreply.github.com>
2025-08-15 20:44:49 +0530

    feat: add unique medium-level Matrix Diagonal Sum problem solution in Java

---
+++ Arrays/MatrixDiagonalSum.java
@@ -0,0 +1,27 @@
+public class MatrixDiagonalSum {
+    public static int diagonalSum(int[][] mat) {
+        int n = mat.length;
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += mat[i][i]; // Primary diagonal
+            sum += mat[i][n - 1 - i]; // Secondary diagonal
+        }
+
+        // If n is odd, subtract the middle element (double-counted)
+        if (n % 2 == 1) {
+            sum -= mat[n / 2][n / 2];
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[][] mat = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        System.out.println("Diagonal Sum: " + diagonalSum(mat));
+    }
+}
