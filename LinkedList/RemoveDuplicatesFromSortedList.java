1d5b61374a4b999e8b2f06dca870e8391567d194
shivaragula <148362743+shivaragula@users.noreply.github.com>
2025-08-18 20:32:41 +0530

    added Java solution for LeetCode 83 - Remove Duplicates from Sorted List with input/output

---
+++ LinkedList/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,43 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
+public class RemoveDuplicatesFromSortedList {
+    public static ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+        while (current != null && current.next != null) {
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return head;
+    }
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(1);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(3);
+        head.next.next.next.next = new ListNode(3);
+
+        System.out.print("Input: ");
+        printList(head);
+
+        head = deleteDuplicates(head);
+
+        System.out.print("Output: ");
+        printList(head);
+    }
+}
